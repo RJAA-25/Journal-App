65569819627c5209dae9068331d98bdd22a687b6
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-07-09 22:05:56 +0800

    Create category controller tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,52 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+  
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category created as \"#{@category.name}\""
+      redirect_to categories_path(@category)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "Category updated as \"#{@category.name}\""
+      redirect_to categories_path(@category)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.delete
+    redirect_to categories_path
+  end
+
+
+  private
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :description, :theme_color)
+  end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @category = Category.create(name: "Some Name", description: "Some description")
+  end
+
+  test "should reach index controller action" do
+    get categories_path
+    assert_response :success
+  end
+
+  test "should reach show controller action" do
+    get categories_path(@category)
+    assert_response :success
+  end
+
+  test "should reach new controller action" do
+    get new_category_path
+    assert_response :success
+  end
+
+  test "should reach create controller action" do
+    assert_difference "Category.count", 1 do
+      post categories_path, params: { category: { name: "Another Name", description: "Some description", theme_color: "#540D6E" } }
+    end
+    assert_redirected_to categories_path(Category.last)
+  end
+
+  test "should reach edit controller action" do
+    get edit_category_path(@category)
+    assert_response :success
+  end
+
+  test "should reach update controller action" do
+    assert_no_difference "Category.count" do
+      patch category_path(@category), params: { category: { name: "New Name", description: "Some description", theme_color: "#540D6E" } }
+    end
+    assert_redirected_to categories_path(@category)
+  end
+
+  test "should reach destroy controller action" do
+    assert_difference "Category.count", -1 do
+      delete category_path(@category)
+    end
+    assert_redirected_to categories_path
+  end
 end
