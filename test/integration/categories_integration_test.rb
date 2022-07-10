7ff56ef6cf228ef8e0ab79d12f2f53c7c3ae639c
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-07-10 21:56:19 +0800

    Add category integration tests

---
+++ test/integration/categories_integration_test.rb
@@ -0,0 +1,46 @@
+require "test_helper"
+
+class BlogFlowTest < ActionDispatch::IntegrationTest
+  setup do
+    @category = Category.create(name: "Category 1", description: "This is a description")
+  end
+
+  test "should create a new category" do
+    get new_category_path
+    assert_response :success
+    assert_difference "Category.count", 1 do
+      post categories_path, params: { category: { name: "Example", description: "This is a description", theme_color: "#540D6E" } }
+      assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+    assert_match "Example", response.body
+  end
+
+  test "should show and update existing category" do
+    get category_path(@category)
+    assert_response :success
+    get edit_category_path(@category)
+    assert_response :success
+    assert_no_difference "Category.count" do
+      patch  category_path(@category), params: { category: { name: "Example", description: "This is a description", theme_color: "#540D6E" } }
+      assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+    assert_match "Example", response.body
+  end
+
+  test "should delete an existing category" do
+    get category_path(@category)
+    assert_response :success
+    assert_difference "Category.count", -1 do
+      delete category_path(@category)
+      assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+    assert_no_match "Category 1", response.body
+  end
+
+end 
