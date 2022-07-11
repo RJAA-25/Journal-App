552b8f70631f0a4f4f109b4145d92eba6402d7a3
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-07-11 21:52:36 +0800

    Add task model tests

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :category
+  validates :name, presence: true, length: { minimum: 4 }
+  validates :details, presence: true
+  validates :deadline, comparison: { greater_than: DateTime.now }
+  validates :completed, inclusion: [true, false]
+  validates :overdue, inclusion: [true, false]
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -2,24 +2,43 @@ require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
   setup do
-    @task = Task.create(name: "Task", details: "Some task details", deadline: DateTime.now + 1, completed: false)
+    @category = Category.create(name: "Some Name", description: "Some description")
+    @task = Task.new(name: "Task", details: "Some task details", deadline: DateTime.now + 1, category_id: @category.id)
   end
 
+  # :name attribute tests
   test "should not save without name" do
-    flunk
+    @task.name = nil
+    assert_not @task.save
   end
 
   test "should not save when name is less than 4 characters" do
-    flunk
+    @task.name = "x" * 3
+    assert_not @task.save
   end
 
+  # :details attribute tests
   test "should not save without details" do
-    flunk
+    @task.details = nil
+    assert_not @task.save
   end
 
+  # :deadline attribute tests
   test "should not save when deadline has passed" do
-    flunk
+    @task.deadline = DateTime.now - 1
+    assert_not @task.save
   end
 
-  test ""
+  # :completed attribute tests
+  test "should not save when :completed is nil" do
+    @task.completed = nil
+    assert_not @task.save
+  end
+  
+  # :overdue attribute tests
+  test "should not save when :overdue is nil" do
+    @task.overdue = nil
+    assert_not @task.save
+  end
+
 end
