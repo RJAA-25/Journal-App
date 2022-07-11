3b342e8f7e03986dd4c323bc93fe3915e83882be
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-07-11 20:23:29 +0800

    Add task model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_08_120801) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_11_121710) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -19,4 +19,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_08_120801) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "details"
+    t.datetime "deadline"
+    t.boolean "completed", default: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_tasks_on_category_id"
+  end
+
+  add_foreign_key "tasks", "categories"
 end
