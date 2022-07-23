22c2ee6d47ff5c6d93f0697c1358de138c9545a4
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-07-23 13:57:23 +0800

    Configure users sessions and registrations controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
   before_action :toggle_overdue, only: [:dashboard]
 
   def home
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "pages#home"
   get "/dashboard" => "pages#dashboard"
+  devise_for :users, module: "users", skip: [:sessions]
+  devise_scope :user do
+    get "login" => "users/sessions#new", as: :new_user_session
+    post "login" => "users/sessions#create", as: :user_session
+    delete "logout" => "users/sessions#destroy", as: :destroy_user_session
+    get "register" => "users/registrations#new", as: :register
+    get ":username/profile" => "users/registrations#edit", as: :profile_edit
+  end
   resources :categories, except: [:index] do
     resources :tasks, except: [:index]
     patch "/tasks/:id/toggle" => "tasks#toggle", as: :task_toggle
