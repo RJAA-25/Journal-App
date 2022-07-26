f38c19cc03f67d457209046181f65ea2c1a7d88b
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-07-26 15:12:12 +0800

    Fix Task model tests

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -2,9 +2,10 @@ require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
   setup do
-    @user = User.create(first_name: "First", last_name: "Last", username: "username", email: "username@email.com ", password: "password")
-    @category = Category.create(name: "Example", description: "A description", user_id: @user.id)
-    @task = Task.new(name: "Task", details: "Some task details", deadline: DateTime.now + 1, category_id: @category.id)
+    @valid_user = User.create(first_name: "Valid", last_name: "Example", username: "validExample", email: "valid_example@email.com", password: "password")
+    @valid_category = Category.create(name: "Valid Category", description: "A description", user_id: @valid_user.id)
+    @valid_task= Task.create(name: "Valid Task", details: "A detail", deadline: DateTime.current + 1.hour, category_id: @valid_category.id)
+    @task = Task.new(name: "Task", details: "Some task details", deadline: DateTime.now + 1, category_id: @valid_category.id)
   end
 
   # :name attribute tests
@@ -59,14 +60,12 @@ class TaskTest < ActiveSupport::TestCase
   # Model Associations
   # Task - User
   test "should get details of user parent" do
-    @task.save
-    assert_equal @task.category.user.username, "username"
-    assert_equal @task.category.user.email, "username@email.com"
+    assert_equal @valid_task.category.user.username, "validExample"
+    assert_equal @valid_task.category.user.email, "valid_example@email.com"
   end
 
   # Task - Category
   test "should get details of category parent" do
-    @task.save
-    assert_equal @task.category.name, "Example"
+    assert_equal @valid_task.category.name, "Valid Category"
   end
 end
